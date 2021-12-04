749cb3e64ccabf1fdc48840767a881208c91b8be
Ido Calman <calman.ido@gmail.com>
2021-12-04 15:41:44 -0500

    Modified version of RCT

--- RandomCalmanTree.py
+++ RandomCalmanTree.py
@@ -30,23 +30,32 @@ class RandomCalmanTree:
 
         # tree construction
         while len(curr_layer) > 1:
-            # next layer size shrinks with density
-            next_layer = []
+            nodes_map = {}
+            edges = []
             next_layer_size = max(1, int(len(curr_layer) * p))
 
-            # create next layer nodes
-            for i in range(next_layer_size):
-                op = random.choice(list(self.operations.keys()))
-                node_props = {"type": op, "val": None, "layer": n_layer}
-                tree.add_nodes_from([(num_nodes, node_props)])
-                next_layer.append(num_nodes)
-                num_nodes += 1
+            # define candidates from next layer
+            next_layer = []
+            next_layer_last = num_nodes + next_layer_size
+            candidates = [x for x in range(num_nodes, next_layer_last)]
 
             # random choice for connections between layers
-            connections = np.random.choice(next_layer, len(curr_layer))
+            connections = np.random.choice(candidates, len(curr_layer))
             for j, src in enumerate(connections):
                 dest = int(curr_layer[j])
-                tree.add_edge(src, dest)
+                edges.append((src, dest))
+
+            # create tree nodes only for nodes with children
+            # add edges to tree
+            for src, dest in edges:
+                if src not in nodes_map.keys():
+                    nodes_map[src] = num_nodes
+                    op = random.choice(list(self.operations.keys()))
+                    node_props = {"type": op, "val": None, "layer": n_layer}
+                    tree.add_nodes_from([(num_nodes, node_props)])
+                    next_layer.append(num_nodes)
+                    num_nodes += 1
+                tree.add_edge(nodes_map[src], dest)
 
             # proceed to next layer
             curr_layer = next_layer
@@ -60,7 +69,7 @@ class RandomCalmanTree:
 
     def _compute(self, node_idx: int):
         node = self.tree.nodes[node_idx]
-        if node['type'] == 'leaf':
+        if node.get('type') == 'leaf':
             return node['val']
 
         # recursively compute values for children nodes
@@ -75,7 +84,7 @@ class RandomCalmanTree:
 
     def compute(self, x: np.array):
         leaves = [n for n, d in self.tree.nodes(data=True)
-                  if d['type'] == 'leaf']
+                  if d.get('type') == 'leaf']
         assert len(x) == len(leaves), f"num_leaves: {len(leaves)}, x: {len(x)}"
 
         # assign input values to leaves
